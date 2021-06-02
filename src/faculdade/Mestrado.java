36ec3cb4135dc4c467b9bec90c4ed5a5c3877105
Andre-penso <82728577+Andre-penso@users.noreply.github.com>
2021-06-02 20:53:59 -0300

    first

--- src/faculdade/Mestrado.java
+++ src/faculdade/Mestrado.java
@@ -3,7 +3,7 @@ package faculdade;
 
 
 public class Mestrado extends Professor{
-	private static final double plus = 3.55;
+	private static final double plus = 3.555;
 	
 	public Mestrado(String nome, int cpf, double salario, double salfinal, String nivel)  {
 		super(nome, cpf, salario, plus, salfinal);
